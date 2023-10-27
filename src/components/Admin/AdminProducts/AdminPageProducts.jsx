0f33d15e1507868f7d64828a702d19f5263cd3a2
um3ra <ereakvlad3@gmail.com>
2023-10-27 18:08:55 +0300

    added pagination

--- src/components/Admin/AdminProducts/AdminPageProducts.jsx
+++ src/components/Admin/AdminProducts/AdminPageProducts.jsx
@@ -12,6 +12,7 @@ import Pagination from "../../Pagination/Pagination";
 const AdminPageProducts = () => {
     const dispatch = useDispatch();
     const products = useSelector(state => state.products.productList);
+    const pagination = useSelector(state => state.products.filter.pagination);
     const [deleteProduct, {data: deleteProductData}] = useDeleteProductMutation();
     const navigate = useNavigate();
 
@@ -24,54 +25,57 @@ const AdminPageProducts = () => {
     }
 
     return (
-        <div className={styles.adminContent}>
-            <div onClick={() => navigate('create-product')}>
-                <Button>
-                    Create new
-                </Button>
+        <div className={styles.adminContentWrapper}>
+            <div className={styles.adminContent}>
+                <div onClick={() => navigate('create-product')}>
+                    <Button>
+                        Create new
+                    </Button>
+                </div>
+                <div className={styles.adminInnerContent}>
+
+                    <ul className={styles.adminContentList}>
+                        <li>id</li>
+                        {products.map(product => {
+                            return <li key={product.id}>{product.id}</li>
+                        })}
+                    </ul>
+
+                    <ul className={styles.adminContentList}>
+                        <li>title</li>
+                        {products.map(product => {
+                            return <li key={product.id}>{product.title}</li>
+                        })}
+                    </ul>
+
+                    <ul className={styles.adminContentList}>
+                        <li>price</li>
+                        {products.map(product => {
+                            return <li key={product.id}>{product.price}$</li>
+                        })}
+                    </ul>
+
+                    <ul className={styles.adminContentList}>
+                        <li>author</li>
+                        {products.map(product => {
+                            return <li key={product.id}>{product.author}</li>
+                        })}
+                    </ul>
+                </div>
+
+                <div className={`${styles.adminContentList} ${styles.adminContentBtns}`}>
+                    <div>Options</div>
+                    {products.map(product => {
+                        return <div key={product.id}>
+                            <button className={styles.adminContentBtnsEdit}
+                                    onClick={() => navigate(`update-product/${product.title}`)}><AiOutlineEdit/></button>
+                            <button className={styles.adminContentBtnsDelete} onClick={() => deleteProduct(product.id)}>
+                                <AiOutlineDelete/></button>
+                        </div>
+                    })}
+                </div>
             </div>
-            <div className={styles.adminInnerContent}>
-
-                <ul className={styles.adminContentList}>
-                    <li>id</li>
-                    {products.map(product => {
-                        return <li key={product.id}>{product.id}</li>
-                    })}
-                </ul>
-
-                <ul className={styles.adminContentList}>
-                    <li>title</li>
-                    {products.map(product => {
-                        return <li key={product.id}>{product.title}</li>
-                    })}
-                </ul>
-
-                <ul className={styles.adminContentList}>
-                    <li>price</li>
-                    {products.map(product => {
-                        return <li key={product.id}>{product.price}$</li>
-                    })}
-                </ul>
-
-                <ul className={styles.adminContentList}>
-                    <li>author</li>
-                    {products.map(product => {
-                        return <li key={product.id}>{product.author}</li>
-                    })}
-                </ul>
-            </div>
-
-            <div className={`${styles.adminContentList} ${styles.adminContentBtns}`}>
-                <div>Options</div>
-                {products.map(product => {
-                    return <div key={product.id}>
-                        <button className={styles.adminContentBtnsEdit}
-                                onClick={() => navigate(`update-product/${product.title}`)}><AiOutlineEdit/></button>
-                        <button className={styles.adminContentBtnsDelete} onClick={() => deleteProduct(product.id)}>
-                            <AiOutlineDelete/></button>
-                    </div>
-                })}
-            </div>
+            <Pagination pagination={pagination}/>
         </div>
     );
 };
