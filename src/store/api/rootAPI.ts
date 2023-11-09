a9035294de4475ad711c77019d2e28f0353519f1
um3ra <ereakvlad3@gmail.com>
2023-11-09 18:45:45 +0300

    typed rootAPI

--- src/store/api/rootAPI.ts
+++ src/store/api/rootAPI.ts
@@ -1,4 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {RootState} from "../store";
 
 export const rootAPI = createApi({
     reducerPath: 'root',
@@ -7,7 +8,7 @@ export const rootAPI = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: process.env.REACT_APP_API_URL,
         prepareHeaders: (headers, {getState}) => {
-            const token = getState().auth.token;
+            const token = (getState() as RootState).auth.token;
             if (token) {
                 headers.set('Authorization', `Bearer ${token}`);
             }
