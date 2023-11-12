1f276a9507de151e194ef2a4714fff08faf9614d
um3ra <ereakvlad3@gmail.com>
2023-11-12 13:34:28 +0300

    updated orderAPI

--- src/store/order/orderAPI.ts
+++ src/store/order/orderAPI.ts
@@ -1,15 +1,15 @@
-import {rootAPI} from "../api/rootAPI";
+import {APIResponse, rootAPI} from "../api/rootAPI";
 import {IOrder} from "../../@types/orderType";
 
 const orderAPI = rootAPI.injectEndpoints({
     endpoints: build => ({
-        getUserOrders: build.query<IOrder[], null>({
+        getUserOrders: build.query<APIResponse<IOrder[]>, null>({
             query() {
                 return "orders/user"
             },
             providesTags: ['Order'],
         }),
-        checkout: build.mutation({
+        checkout: build.mutation<APIResponse<string>, null>({
             query() {
                 return {
                     url: 'orders',
